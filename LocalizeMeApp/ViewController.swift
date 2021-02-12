8f4d384be9a41d602bf538f7995a70c5374876f8
Dmitriy Kara <kara@group-ib.com>
2021-02-12 23:53:19 +0300

    Complete the task

--- LocalizeMeApp/ViewController.swift
+++ LocalizeMeApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var localeLabel: UILabel!
+    @IBOutlet var flagImageView: UIImageView!
+    @IBOutlet var labels: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let locale = NSLocale.current
+        let currentLangID = NSLocale.preferredLanguages[0]
+        let displayLang = (locale as NSLocale).displayName(forKey: NSLocale.Key.languageCode, value: currentLangID)
+        let capitalized = displayLang?.capitalized(with: locale)
+
+        localeLabel.text = capitalized
+        labels[0].text = NSLocalizedString("LABEL_ONE", comment: "The number 1")
+        labels[1].text = NSLocalizedString("LABEL_TWO", comment: "The number 2")
+        labels[2].text = NSLocalizedString("LABEL_THREE", comment: "The number 3")
+        labels[3].text = NSLocalizedString("LABEL_FOUR", comment: "The number 4")
+        labels[4].text = NSLocalizedString("LABEL_FIVE", comment: "The number 5")
+
+        let flagFile = NSLocalizedString("FLAG_FILE", comment: "Name of the flag")
+        flagImageView.image = UIImage(named: flagFile)
     }
-
-
 }
 
